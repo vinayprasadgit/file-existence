8d9479952b5d089145f063446a5d8f96a7707bd3
vinayprasadgit <87464583+vinayprasadgit@users.noreply.github.com>
2022-01-03 11:51:35 +0530

    Create uatInput.groovy

---
+++ src/com/mcnz/uatInput.groovy
@@ -0,0 +1,14 @@
+package com.mcnz
+
+  public class uatInput {
+    def buildIsUatApproved() {
+    def file = new File("C:/_tools/approved.txt")
+    if (file.exists()){
+      return true;
+    }
+    else {
+      println "Approval file does not exist."
+    } 
+    return false; 
+  }
+}
